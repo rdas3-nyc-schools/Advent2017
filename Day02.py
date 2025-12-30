bb94c59cc79ede3bb71d140391382821f300afbb
rdas3-nyc-schools <98552214+rdas3-nyc-schools@users.noreply.github.com>
2025-12-30 18:12:10 -0500

    Add files via upload

--- Day02.py
+++ Day02.py
@@ -17,10 +17,7 @@ part_one_answer += sum(max(numbers) - min(numbers) for numbers in number_lists)
 
 print("Part one answer:", part_one_answer)
 
-part_two_answer = 0
-
-part_two_answer += sum(int(n1 / n2) for number in number_lists for n1 in number for n2 in number
-                       if n1 != n2 and n1 % n2 == 0)
+part_two_answer = sum(int(n1 / n2) for number in number_lists for n1 in number for n2 in number
+                      if n1 != n2 and n1 % n2 == 0)
 
 print("Part two answer:", part_two_answer)
-
